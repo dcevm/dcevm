f4d5de965bd90f7c6cd7d912253b9e365313914d
Ivan Dubrov <dubrov.ivan@gmail.com>
2013-04-04 13:02:14 -0700

    Fixing early bailout from the compiler and commenting it out (to try if we still need it)

--- src/share/vm/compiler/compileBroker.cpp
+++ src/share/vm/compiler/compileBroker.cpp
@@ -1181,9 +1181,10 @@ nmethod* CompileBroker::compile_method(methodHandle method, int osr_bci,
                                        int comp_level,
                                        methodHandle hot_method, int hot_count,
                                        const char* comment, Thread* THREAD) {
-  if (((CompilerThread *)Thread::current())->should_bailout()) {
-    return NULL; // FIXME: DCEVM: should we do something else?
-  }
+//  JavaThread* thread = JavaThread::current();
+//  if (thread->is_Compiler_thread() && thread->as_CompilerThread()->should_bailout()) {
+//    return NULL; // FIXME: DCEVM: should we do something else?
+//  }
 
   // make sure arguments make sense
   assert(method->method_holder()->klass_part()->oop_is_instance(), "not an instance method");

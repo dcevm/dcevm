25ec3e2061f4e304f13923702354b75bb77e7809
Ivan Dubrov <idubrov@guidewire.com>
2014-04-25 16:08:45 -0700

    Fixing native code compilation

--- native/compile.cmd
+++ native/compile.cmd
@@ -1,4 +1,4 @@
 set WINDOWS_SDK=c:\Program Files\Microsoft SDKs\Windows\v7.1
 call "%WINDOWS_SDK%\bin\setenv.cmd" /%ARCH%
 
-%VC%\bin\cl /I%JAVA_HOME%\include /I%JAVA_HOME%\include\win32 natives.c /Fenatives.dll /LD
+cl /I%JAVA_HOME%\..\include /I%JAVA_HOME%\..\include\win32 natives.c /Febuild\natives.dll /LD
